271eb1b78557d1c289fe11a9b3754e785a477464
Iqra <55846879+Iqramanzoor58@users.noreply.github.com>
2021-06-14 14:24:17 +0500

    Create login.php

---
+++ Front End Dev/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>LogIN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form>
+        <h1>Log In</h1>
+        <label>User Name</label>
+        <input type="text" id=user name="user" placeholder="User Name">
+        <label>Password</label>
+        <input type="password" id=password name="password" placeholder="password">
+        <button type="submit">LOG IN</button>
+        <p>Don't have an account? <a href="register.php">Register now</a>.</p>
+    </form>
+</body>
