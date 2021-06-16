82a9dabea32f29d1e5c0b190f3d47631b5a37a65
Iqra <55846879+Iqramanzoor58@users.noreply.github.com>
2021-06-16 10:35:10 +0500

    Update login.php

--- Front End Dev/login.php
+++ Front End Dev/login.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="utf-8">
     <title>LogIN</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <form>
+        <img src="Person.png" class="img"> 
         <h1>Log In</h1>
+        <p>Please Fill this form to Log into your account</p>
         <label>User Name</label>
-        <input type="text" id=user name="user" placeholder="User Name">
+        <input type="text" id=user name="user" placeholder="@User Name">
         <label>Password</label>
         <input type="password" id=password name="password" placeholder="password">
         <button type="submit">LOG IN</button>
-        <p>Don't have an account? <a href="register.php">Register now</a>.</p>
+        <p>Don't have an account? <a href="register.html">Register now</a>.</p>
     </form>
 </body>
