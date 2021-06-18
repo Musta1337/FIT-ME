db5bfccad6c08fc954992d440a5bf7dda5f4514f
Iqra <55846879+Iqramanzoor58@users.noreply.github.com>
2021-06-18 09:26:16 +0500

    Update Register.php

--- Front End Dev/Register.php
+++ Front End Dev/Register.php
@@ -18,5 +18,30 @@
             <button type="submit">SIGN UP</button>
             <p>Already have an account? <a href="login.html">LOG IN</a>.</p>
         </form>
+        <?php  // creating a database connection 
+    if(isset($_POST["M_password"]))
+    {
+        $db_sid = "(DESCRIPTION =
+        (ADDRESS = (PROTOCOL = TCP)(HOST = DESKTOP-UAQMM7R)(PORT = 1521))
+        (CONNECT_DATA =
+        (SERVER = DEDICATED)
+        (SERVICE_NAME = iqra)
+        ))";// Your oracle SID, can be found in tnsnames.ora  ((oraclebase)\app\Your_username\product\11.2.0\dbhome_1\NETWORK\ADMIN)
+        $db_user = "scott";   // Oracle username e.g "scott"
+        $db_pass = "1234";    // Password for user e.g "1234"
+        $con = oci_connect($db_user,$db_pass,$db_sid); 
+        
+    if($con) 
+        { echo "Oracle Connection Successful."; } 
+    else 
+        { die('Could not connect to Oracle: '); } 
+    $q = "INSERT INTO Member (M_password,Age,gender)
+    VALUES (".$_POST["M_password"].", ".$_POST["Age"].", ".$_POST["Gender"].")";
+    $query_id = oci_parse($con, $q); 		
+    $r = oci_execute($query_id);
+    echo"<br>";
+    echo"Your Record has been Inserted Successfully.Now you are Member of FITME";
+    }
+ ?>
     </body>
 </html>
