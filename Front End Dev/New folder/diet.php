9394f1f49f19499e9ee42f9e216382dc3d1275fe
Iqra <55846879+Iqramanzoor58@users.noreply.github.com>
2021-06-19 20:15:49 +0500

    Update diet.php

--- Front End Dev/New folder/diet.php
+++ Front End Dev/New folder/diet.php
@@ -7,7 +7,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <title>Find Diet Plan</title>
-        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="plan.css">
     </head>
     <body>
         <form action="diet.php" method="post">
@@ -20,13 +20,14 @@ session_start();
         <?php  // creating a database connection 
     if(isset($_POST["type"]))
     {
-        $db_sid = "  (DESCRIPTION =
-        (ADDRESS=(PROTOCOL=tcp)(HOST=MUSTA-PC)(PORT=1521))
-        (CONNECT_DATA =
-          (SERVER = DEDICATED)
-          (SERVICE_NAME = test)
-        )
-      )";// Your oracle SID, can be found in tnsnames.ora  ((oraclebase)\app\Your_username\product\11.2.0\dbhome_1\NETWORK\ADMIN)
+        $db_sid = " 
+        (DESCRIPTION =
+          (ADDRESS = (PROTOCOL = TCP)(HOST = DESKTOP-UAQMM7R)(PORT = 1521))
+          (CONNECT_DATA =
+            (SERVER = DEDICATED)
+            (SERVICE_NAME = iqra)
+          )
+        )";// Your oracle SID, can be found in tnsnames.ora  ((oraclebase)\app\Your_username\product\11.2.0\dbhome_1\NETWORK\ADMIN)
         $db_user = "fitadmin";   // Oracle username e.g "scott"
         $db_pass = "fitness";    // Password for user e.g "1234"
         $con = oci_connect($db_user,$db_pass,$db_sid); 
